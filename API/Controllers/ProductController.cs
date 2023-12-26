18c660b6822580eadaacfd738933ddc5058e01a0
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2023-12-26 15:19:24 -0300

    Initial

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet()]
+        public async Task<List<Product>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+    }
+}
