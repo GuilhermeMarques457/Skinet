62abade5190e8901ee14d538c3a7131f911f886e
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2024-01-05 21:03:25 -0300

    Finish Section 5 - Error Handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,45 @@
+﻿using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseAPIController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BuggyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("not-found")] 
+        public ActionResult GetNotFound() 
+        {
+            var thing = _context.Products.Find(42);
+
+            return thing == null ? NotFound(new ApiResponse(404)) : Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+            var thingToReturn = thing.ToString();
+
+            return thing == null ? NotFound(new ApiResponse(500)) : Ok(thing);
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("bad-request/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+﻿
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+            StatusCode = statusCode;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad Request, you have made",
+                401 => "Unauthorized, you are not",
+                404 => "Resouce found, it was not",
+                500 => "Server error, contact support",
+                _ => "Unescpected Error"
+            };
+        }
+
+    }
+}
